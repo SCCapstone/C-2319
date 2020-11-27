aa6eaa70521f23ee71825100d24c93db1dd13a49
Erik_N <Eriknick24@gmail.com>
2020-11-27 03:01:33 -0500

    Solving email send issue

--- register/urls.py
+++ register/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^profile/edit/$' , views.edit_profile, name = 'edit_profile'),
     url(r'^change-password$' , views.change_password, name = 'change_password'),
     url(r'^delete/$', views.remove_user, name= "remove_user"),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    #url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            #views.activate, name='activate'),
 ]
